2da1f664eaa61210650d720acdeb9bc3962b4b15
nishadagar162-dev <nishadagar162@gmail.com>
2026-09-13 23:24:51 +0530

    Add two implementations of odd sum calculation
    Added two identical implementations of a program that calculates the sum of the first n odd numbers.

---
+++ for.c
@@ -0,0 +1,37 @@
+// day 14
+// code 27
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + (2 * i - 1);
+    }
+
+    printf("Sum=%d", sum);
+
+    return 0;
+}
+//code 28
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + (2 * i - 1);
+    }
+
+    printf("Sum=%d", sum);
+
+    return 0;
+}
