c2745a8bd9ce0434106e0da3e4cda22b25818535
nishadagar162-dev <nishadagar162@gmail.com>
2026-09-15 10:14:09 +0530

    Add two C programs for printing patterns

---
+++ printf.c
@@ -0,0 +1,37 @@
+//day26
+//code 51
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = i; j <= 5; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+//code 52
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
