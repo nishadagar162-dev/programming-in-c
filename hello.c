2576fbafcd0483df22ebdc2e1a3956aff6b90e29
nishadagar162-dev <nishadagar162@gmail.com>
2026-09-15 10:11:53 +0530

    Implement pattern printing in hello.c
    Added two programs that print patterns using nested loops.

---
+++ hello.c
@@ -0,0 +1,38 @@
+//day 24
+//code 47
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+//code 48
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+    return 0;
+}
